4c6577ddf1f28d83df8befdae1706d65f0939132
Shivam <shivam108ku@gmail.com>
2026-03-24 17:57:34 +0530

    feat: create booking services function complete

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -1,11 +1,37 @@
-export async function createBookingServices () {
+import { createBooking, createIdempotencyKey } from "../repositories/booking.repository";
+import { generateIdempotencyKey } from "../utils/generateIdempotencyKey";
+
+
+export async function createBookingService (
+    userId: number,
+    hotelId: number,
+    totalGuests: number,
+    bookingAmount: number,
+    checkInDate: Date,
+) {
+   //  Implement the login here
+   const booking = await createBooking({
+    userId,
+    hotelId,
+    totalGuests: totalGuests,
+    bookingAmount: bookingAmount,
+   });
+
+   const idempotencyKey = generateIdempotencyKey();
+   await createIdempotencyKey(idempotencyKey, booking.id)
+
+   return {
+    bookingId: booking.id,
+    idempotencyKey: idempotencyKey,
+   }
 
 }
 
-export async function finalizeBookingServices () {
+
+export async function finalizeBookingService () {
     
 }
 
-export async function finalizeBookingServices () {
+// export async function finalizeBookingService () {
     
-}
+// }
